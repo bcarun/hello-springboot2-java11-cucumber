c24ceb8df5466acff8210b60bb1be300b12e815a
Arun Chandrasekaran <achandrasekaran@wfscorp.com>
2020-10-17 13:40:46 -0400

    Using lambda stepdefs and java11

--- src/test/java/com/arun/cucumber/EmployeeStepDefs.java
+++ src/test/java/com/arun/cucumber/EmployeeStepDefs.java
@@ -3,9 +3,8 @@ package com.arun.cucumber;
 import static io.restassured.RestAssured.given;
 
 import com.arun.cucumber.Startup.Employee;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java8.En;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -13,7 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
 
-public class EmployeeStepDefs {
+public class EmployeeStepDefs implements En {
 
   @LocalServerPort
   private int port;
@@ -21,35 +20,37 @@ public class EmployeeStepDefs {
   @Autowired
   private TestContext testContext;
 
-  @Given("user wants to create a employee with following attributes")
-  public void user_wants_to_create_a_employee_with_following_attributes(List<Employee> employees) {
-    final RequestSpecification request = given().contentType(ContentType.JSON)
-        .accept(ContentType.JSON)
-        .body(employees.get(0))
-        .log()
-        .all();
+  public EmployeeStepDefs() {
 
-    testContext.setRequest(request);
-  }
+    Given("user wants to create a employee with following attributes", ( DataTable dataTable) -> {
+      List<Employee> employees = dataTable.asList(Employee.class);
 
-  @When("user saves employee")
-  public void user_saves_employee() {
-    String url = "http://localhost:" + port + "/employees";
-    final Response response = testContext.getRequest()
-        .post(url)
-        .thenReturn();
+      final RequestSpecification request = given().contentType(ContentType.JSON)
+          .accept(ContentType.JSON)
+          .body(employees.get(0))
+          .log()
+          .all();
 
-    response.then()
-        .log()
-        .all();
+      testContext.setRequest(request);
+    });
 
-    testContext.setResponse(response);
-  }
+    When("user saves employee", () -> {
+      String url = "http://localhost:" + port + "/employees";
+      final Response response = testContext.getRequest()
+          .post(url)
+          .thenReturn();
 
-  @Then("the save is successful")
-  public void the_save_is_successful() {
-    testContext.getResponse()
-        .then()
-        .statusCode(200);
+      response.then()
+          .log()
+          .all();
+
+      testContext.setResponse(response);
+    });
+
+    Then("the save is successful", () -> {
+      testContext.getResponse()
+          .then()
+          .statusCode(200);
+    });
   }
 }
